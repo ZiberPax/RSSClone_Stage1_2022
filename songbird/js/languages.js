76d845517a5d1acf89a92fb5e9bee6c8e6113334
Alexey M <323p@mail.ru>
2022-11-21 22:14:53 +0300

    feat: delete volume button

---
+++ songbird/js/languages.js
@@ -0,0 +1,20 @@
+const languages = {
+  ["en"]: {
+    title: 'Your Quize Is Waiting For You'
+  },
+  ["ru"]: {
+    title: 'Твой Квиз Ждет Тебя'
+  }
+  
+}
+
+
+
+const button = document.querySelector(".footer_button_language");
+
+function changeLang (lang) {
+  d
+}
+
+changeLang
+button.addEventListener("click", changeLang)
